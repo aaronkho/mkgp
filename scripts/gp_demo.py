cb988a6de7986d3361a765282cd53c4d91d4c0c5
Aaron <aaron.k.hohoho@gmail.com>
2017-11-16 15:03:19 +0000

    Version m3.03, fixed bugs in demonstration script
    Swapped set functions in demo for newer specifications, otherwise fatal error
    Improved comments within demo script, gp_demo.py

--- scripts/gp_demo.py
+++ scripts/gp_demo.py
@@ -13,7 +13,8 @@ pdir = './GPPlots/'
 if not os.path.isdir(pdir):
     os.makedirs(pdir)
 
-# Data generation
+
+# Data generation - not real fusion data!
 
 xsig = 0.01
 ysig = 0.25
@@ -32,35 +33,66 @@ yy = yy + ysig * np.random.randn(yy.size)
 xe = np.full(xx.shape,xsig)
 ye = np.full(yy.shape,ysig)
 
+
+
 # Fitting
 
+# Define kernels
 xn = np.linspace(0.0,1.0,100)
 kernel = GPR1D.RQ_Kernel(1.0e0,1.0e0,1.0e0)
 kbounds = np.atleast_2d([[1.0e-1,1.0e-2,1.0e0],[1.0e1,1.0e0,1.0e1]])
 nkernel = GPR1D.Sum_Kernel(GPR1D.RQ_Kernel(1.0e0,1.0e0,1.0e0),GPR1D.Noise_Kernel(1.0e-2))
 nkbounds = np.atleast_2d([[1.0e-1,1.0e-1,1.0e-1,1.0e-3],[1.0e1,1.0e0,1.0e1,1.0e-1]])
+
+# GPR fit accounting only for y-errors
+# Define fit settings
 GPR = GPR1D.GPR1D()
 GPR.set_kernel(kernel=kernel,kbounds=kbounds,regpar=1.0)
-GPR.set_error_kernel(kernel=nkernel,kbounds=nkbounds,regpar=10.0,epsilon=1.0e-1)
+GPR.set_error_kernel(kernel=nkernel,kbounds=nkbounds,regpar=10.0)
 GPR.set_raw_data(xdata=xx,ydata=yy,yerr=ye,xerr=xe,dxdata=[0.0],dydata=[0.0],dyerr=[0.0])
 GPR.set_search_parameters(epsilon=1.0e-3)
+GPR.set_error_search_parameters(epsilon=1.0e-1)
+GPR.set_conditioner(condnum=1.0e-3)    # Defines minimum spacing between neighbouring points before mixing them
+
+# Uncomment these to test recommended optimizers
+#GPR.set_search_parameters(epsilon=1.0e-3,method='adadelta')
+#GPR.set_error_search_parameters(epsilon=1.0e-1,method='adam')
+
+# Do fit
 GPR.GPRFit(xn,nrestarts=5)
+
+# Grab outputs
 (gpname,gppars) = GPR.get_gp_kernel_details()
 (egpname,egppars) = GPR.get_gp_error_kernel_details()
 (vgp,egp,vdgp,edgp) = GPR.get_gp_results()
 
+# GPR fit accounting for y-errors AND x-errors
+# Define fit settings
 NIGPR = GPR1D.GPR1D()
 NIGPR.set_kernel(kernel=kernel,kbounds=kbounds,regpar=1.0)
-NIGPR.set_error_kernel(kernel=nkernel,kbounds=nkbounds,regpar=10.0,epsilon=1.0e-1)
+NIGPR.set_error_kernel(kernel=nkernel,kbounds=nkbounds,regpar=10.0)
 NIGPR.set_raw_data(xdata=xx,ydata=yy,yerr=ye,xerr=xe,dxdata=[0.0],dydata=[0.0],dyerr=[0.0])
 NIGPR.set_search_parameters(epsilon=1.0e-3)
+NIGPR.set_error_search_parameters(epsilon=1.0e-1)
+NIGPR.set_conditioner(condnum=1.0e-3)    # Defines minimum spacing between neighbouring points before mixing them
+
+# Uncomment these to test recommended optimizers
+#NIGPR.set_search_parameters(epsilon=1.0e-3,method='adadelta')
+#NIGPR.set_error_search_parameters(epsilon=1.0e-1,method='adam')
+
+# Do fit
 NIGPR.GPRFit(xn,nigp_flag=True,nrestarts=5)
+
+# Grab outputs
 (nigpname,nigppars) = NIGPR.get_gp_kernel_details()
 (enigpname,enigppars) = NIGPR.get_gp_error_kernel_details()
 (vnigp,enigp,vdnigp,ednigp) = NIGPR.get_gp_results()
 
+
+
 # Plotting
 
+# Raw data with GPR fit and error, only accounting for y-errors
 psig = 2.0
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -73,6 +105,7 @@ ax.set_xlim(0.0,1.0)
 fig.savefig(pdir+'gp_test.png')
 plt.close(fig)
 
+# Derivative of GPR fit and error, only accounting for y-errors
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(xn,vdgp,color='r')
@@ -83,6 +116,7 @@ ax.set_xlim(0.0,1.0)
 fig.savefig(pdir+'gp_dtest.png')
 plt.close(fig)
 
+# Raw data with GPR fit and error, comparison of only y-errors against y-errors AND x-errors
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.errorbar(nxx,yy,xerr=psig*xsig,yerr=psig*ysig,ls='',marker='.',color='k')
@@ -98,6 +132,7 @@ ax.set_xlim(0.0,1.0)
 fig.savefig(pdir+'nigp_test.png')
 plt.close(fig)
 
+# Derivative of GPR fit and error, comparison of only y-errors against y-errors AND x-errors
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(xn,vdgp,color='r')
