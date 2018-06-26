fd7131c33f171556ba7139b7b1b05a370728fbdd
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2018-06-26 14:23:11 +0200

    Updated conda build and meta information

--- build_and_distribute.sh
+++ build_and_distribute.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Package specific settings
+CONDA_PACKAGE_NAME=gpr1d
+
 # Conda
 # Check environment
 conda info > /dev/null
@@ -6,16 +9,15 @@ if [ $? -ne 0 ]
 then
     echo "Unable to run 'conda'. Please install it with your system package manager"; exit 1
 fi
-conda build > /dev/null
-anaconda whoami > /dev/null
+conda build --help > /dev/null
+anaconda --help > /dev/null 2&>1
 if [ $? -ne 0 ]
 then
     echo "Unable to run 'anaconda'. Please install it using 'conda install anaconda-client'"; exit 1
 fi
 
-CONDA_BUILD=conda_build
+CONDA_BUILD_DIR=conda-build
 CURRENT_PLATFORM=$(conda info --json | grep platform | cut -d'"' -f4)
-CONDA_PACKAGE_NAME=gpr1d
 declare -a PYTHON_VERSIONS=("2.7" "3.5" "3.6")
 declare -a PLATFORMS=("linux-64" "osx-64")
 
@@ -23,24 +25,24 @@ declare -a PLATFORMS=("linux-64" "osx-64")
 for PYTHON_VERSION in "${PYTHON_VERSIONS[@]}"
 do
     echo Building $PYTHON_VERSION
-    conda build --output-folder $CONDA_BUILD --python $PYTHON_VERSION .
+    conda build --output-folder $CONDA_BUILD_DIR --python $PYTHON_VERSION .
 done
 
 # Build for all architectures
-for FILE in `find $CONDA_BUILD/$CURRENT_PLATFORM -name $CONDA_PACKAGE_NAME'*' -type f`
+for FILE in `find $CONDA_BUILD_DIR/$CURRENT_PLATFORM -name $CONDA_PACKAGE_NAME'*' -type f`
 do
     for PLATFORM in "${PLATFORMS[@]}"
     do
         echo Converting $FILE for platform $PLATFORM
-        conda convert --platform $PLATFORM $FILE -o $CONDA_BUILD
+        conda convert --platform $PLATFORM $FILE -o $CONDA_BUILD_DIR
     done
     echo $FILE
 done
 
 # Push to conda
-for FILE in `find $CONDA_BUILD -name $CONDA_PACKAGE_NAME'*' -type f`
+for FILE in `find $CONDA_BUILD_DIR -name $CONDA_PACKAGE_NAME'*' -type f`
 do
     echo Uploading $FILE
-    anaconda upload $FILE
+    anaconda upload $FILE --force
 done
 
