3c55e6a52af745940f434b928592cc2aca763eaf
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2018-06-30 01:48:49 +0200

    Moved to noarch package

--- build_and_distribute.sh
+++ build_and_distribute.sh
@@ -17,32 +17,18 @@ then
 fi
 
 CONDA_BUILD_DIR=conda-build
+ANACONDA_USER=aaronkho
 CURRENT_PLATFORM=$(conda info --json | grep platform | cut -d'"' -f4)
 declare -a PYTHON_VERSIONS=("2.7" "3.5" "3.6")
 declare -a PLATFORMS=("linux-64" "osx-64")
 
-# Build for all python version
-for PYTHON_VERSION in "${PYTHON_VERSIONS[@]}"
-do
-    echo Building $PYTHON_VERSION
-    conda build --output-folder $CONDA_BUILD_DIR --python $PYTHON_VERSION .
-done
-
-# Build for all architectures
-for FILE in `find $CONDA_BUILD_DIR/$CURRENT_PLATFORM -name $CONDA_PACKAGE_NAME'*' -type f`
-do
-    for PLATFORM in "${PLATFORMS[@]}"
-    do
-        echo Converting $FILE for platform $PLATFORM
-        conda convert --platform $PLATFORM $FILE -o $CONDA_BUILD_DIR
-    done
-    echo $FILE
-done
+# Build. This package is pure-python, so only one build necessary
+rm -rf $CONDA_BUILD_DIR
+conda build --output-folder $CONDA_BUILD_DIR .
 
 # Push to conda
 for FILE in `find $CONDA_BUILD_DIR -name $CONDA_PACKAGE_NAME'*' -type f`
 do
     echo Uploading $FILE
-    anaconda upload $FILE --force
+    anaconda upload $FILE --force --user $ANACONDA_USER
 done
-
